8b7007cfb685b1f80ec49d8c5a818534aec21f3e
Dominika Czarnecka <s27362@pjwstk.edu.pl>
2022-11-06 09:13:21 +0100

    new project

---
+++ PIP/src/Zadanie3/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+package Zadanie3;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj x");
+        int x = scanner.nextInt();
+        System.out.println("Podaj y");
+    }
+}
